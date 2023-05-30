769189f9112d7c2803432307e0aa2a63dc8c8f46
johnbrightagbons <brightinojohnny@gmail.com>
2023-05-30 18:37:59 +0100

    Added contents

---
+++ about_me/scripts/main.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/Johnny.jpg') {
+    myImage.setAttribute ('src','images/Johnny.jpg');
+  } else {
+    myImage.setAttribute ('src','images/Johnny.jpg');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Thanks for reading, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
